d5529f305b133bf68394ce7176ed591fb2d9a487
Viktoria311 <kotova-viktorija@mail.ru>
2021-06-05 17:06:56 +0300

    feat: 2 exercises

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,137 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+	if self >= 4000 && self <= 0 {
+		return nil
+	}	
+var number = self
+var mystring = ""
+
+if number/1000 >= 1 {
+		if number/1000 ==1 {
+			mystring = "M"
+			number -= 1000
+		}
+		if number/1000 ==2 {
+			mystring = "MM"
+			number -=2000
+		}
+		if number/1000 ==3 {
+			mystring = "MMM"
+			number -=3000
+		}
+}
+if number == 0 { return mystring }
+ if number/100 >= 1 {
+		if number/100 ==1 {
+			mystring += "C"
+			number -=100
+		}
+		if number/100 ==2 {
+			mystring += "CC"
+			number -=200
+		}
+		if number/100 ==3 {
+			mystring += "CCC"
+			number -=300
+		}
+		if number/100 ==4 {
+			mystring += "CD"
+			number -=400
+		}
+		if number/100 ==5 {
+			mystring += "D"
+			number -=500
+		}
+		if number/100 ==6 {
+			mystring += "DC"
+			number -=600
+		}
+		if number/100 ==7 {
+			mystring += "DCC"
+			number -=700
+		}
+		if number/100 ==8 {
+			mystring += "DCCC"
+			number -=800
+		}
+		if number/100 ==9 {
+			mystring += "CM"
+			number -=900
+		}
+}
+if number == 0 { return mystring }
+if number/10 >=1 {
+		if number/10 ==1 {
+			mystring += "X"
+			number -=10
+		}
+		if number/10 ==2 {
+			mystring += "XX"
+			number -=20
+		}
+		if number/10 ==3 {
+			mystring += "XXX"
+			number -=30
+		}
+		if number/10 ==4 {
+			mystring += "XL"
+			number -=40
+		}
+		if number/10 ==5 {
+			mystring += "L"
+			number -=50
+		}
+		if number/10 ==6 {
+			mystring += "LX"
+			number -=60
+		}
+		if number/10 ==7 {
+			mystring += "LXX"
+			number -=70
+		}
+		if number/10 ==8 {
+			mystring += "LXXX"
+			number -=80
+		}
+		if number/10 ==9 {
+			mystring += "XC"
+			number -=90
+		}
+}
+if number == 0 { return mystring}
+		if number ==1 {
+			mystring += "I"
+		}
+		if number ==2 {
+			mystring += "II"
+		}
+		if number ==3 {
+			mystring += "III"
+		}
+		if number ==4 {
+			mystring += "IV"
+		}
+		if number ==5 {
+			mystring += "V"
+		}
+		if number ==6 {
+			mystring += "VI"
+		}
+		if number ==7 {
+			mystring += "VII"
+		}
+		if number ==8 {
+			mystring += "VIII"
+		}
+		if number ==9 {
+			mystring += "IX"
+		}
+		if number ==10 {
+			mystring += "X"
+		}
+	}
+
+        return mystring
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,41 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+
+if image.lengh<row || image[0].length < column { return image }
+
+var arr = image
+arr[row-1][column-1] = newColor
+//пока вдоль одинаковые. надо 2 цикла: вперед и назад
+
+for ( int i = column-1; i< image[0].length; ++i) {
+//это цикл вперед
+if image[row-1][i] == image[row-1][column-1] {
+	arr[row-1][i] = newColor
+	//если совпадает, идем сравнивать сверху и снизу 
+	for (int j = row-1; j>=0; --j) {
+		if image[j][i] == image[row-1][column-1] {arr[j][i] = newColor }
+		}
+	for (int j = row-1; j<image.length; ++j) {
+		if image[j][i] == image[row-1][column-1] {arr[j][i] = newColor }
+		}
+	}
+}
+
+for ( int i = column-1; i>= 0; --i) { //это цикл назад
+if image[row-1][i] == image[row-1][column-1] {
+arr[row-1][i] = newColor 
+//если совпадает, идем сравнивать сверху и снизу 
+	for (int j = row-1; j>=0; --j) {
+		if image[j][i] == image[row-1][column-1] {arr[j][i] = newColor }
+		}
+	for (int j = row-1; j<image.length; ++j) {
+		if image[j][i] == image[row-1][column-1] {arr[j][i] = newColor }
+		}
+}
+}//закрываем цикл назад
+
+
+return arr
     }
 }
